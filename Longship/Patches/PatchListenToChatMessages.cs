9e1e5d9080a0c187bbe1a37b01a38584f694a325
AlexMog <alexmog@live.fr>
2021-02-20 06:06:35 +0100

    Basic commands management

--- Longship/Patches/PatchListenToChatMessages.cs
+++ Longship/Patches/PatchListenToChatMessages.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text.RegularExpressions;
 using HarmonyLib;
 using UnityEngine;
 
@@ -7,11 +8,24 @@ namespace Longship.Patches
     [HarmonyPatch]
     public class PatchListenToChatMessages
     {
+        private static Regex CommandRegex;
+
+        static PatchListenToChatMessages()
+        {
+            CommandRegex = new Regex(@"\/(?<command>[A-Za-z]+) {0,1}(?<argument>.*)", RegexOptions.Compiled);
+        }
+        
         [HarmonyPrefix]
         [HarmonyPatch(typeof(Chat), "RPC_ChatMessage")]
         static void PatchOnNewChatMessage(long sender, Vector3 position, int type, string name, string text)
         {
-            Longship.Log($"RPC_ChatMessage(SenderId: {sender}, Type: {type}, Name: {name}, Text: {text}");
+            var match = CommandRegex.Match(text);
+            if (match.Success)
+            {
+                var command = match.Groups["command"].Value;
+                var argument = match.Groups["argument"].Success ? match.Groups["argument"].Value : null;
+                Longship.Instance.CommandsManager.OnCommandExecuted(command, argument);
+            }
         }
     }
 }

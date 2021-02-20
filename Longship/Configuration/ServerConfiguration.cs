6ee9f4688da5d27792e8968304b13ea466833b12
AlexMog <alexmog@live.fr>
2021-02-20 19:14:15 +0100

    Override server info

--- Longship/Configuration/ServerConfiguration.cs
+++ Longship/Configuration/ServerConfiguration.cs
@@ -3,9 +3,11 @@
     public class ServerConfiguration
     {
         public string ServerName { get; set; } = "Default Server";
+        public string ServerPassword { get; set; } = "";
         public uint MaxPlayers { get; set; } = 10;
         public NetworkConfiguration Network { get; set; } = new NetworkConfiguration();
-        
+        public int ServerPort { get; set; } = 2456;
+
         public class NetworkConfiguration
         {
             public uint DataPerSeconds { get; set; } = 245760;
---
+++ Longship/Patches/PatchServerInfo.cs
@@ -0,0 +1,19 @@
+﻿using HarmonyLib;
+
+namespace Longship.Patches
+{
+    [HarmonyPatch]
+    public class PatchServerInfo
+    {
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(ZNet), "Awake")]
+        static void PatchZNetAwake(ref int ___m_serverPlayerLimit, ref string ___m_ServerName,
+            ref string ___m_serverPassword, ref int ___m_hostPort)
+        {
+            ___m_hostPort = Longship.Instance.ConfigurationManager.Configuration.ServerPort;
+            ___m_serverPassword = Longship.Instance.ConfigurationManager.Configuration.ServerPassword;
+            ___m_ServerName = Longship.Instance.ConfigurationManager.Configuration.ServerName;
+            ___m_serverPlayerLimit = (int) Longship.Instance.ConfigurationManager.Configuration.MaxPlayers;
+        }
+    }
+}

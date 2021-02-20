ac66f6c34db83a3a6f168be53631053cad9578b5
AlexMog <alexmog@live.fr>
2021-02-20 17:20:31 +0100

    Patch to force server max players using Longship configuration file instead

---
+++ Longship/Patches/PatchPlayerCountLimit.cs
@@ -0,0 +1,23 @@
+﻿using HarmonyLib;
+using Steamworks;
+
+namespace Longship.Patches
+{
+    [HarmonyPatch]
+    public class PatchPalyerCountLimit
+    {
+        [HarmonyPostfix]
+        [HarmonyPatch(typeof(ZNet), "Awake")]
+        static void PatchZNetMaxPlayers(ref int ___m_serverPlayerLimit)
+        {
+            ___m_serverPlayerLimit = (int) Longship.Instance.ConfigurationManager.Configuration.MaxPlayers;
+        }
+
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(SteamGameServer), "SetMaxPlayerCount")]
+        static void PatchSteamGameServerMaxPlayers(ref int cPlayersMax)
+        {
+            cPlayersMax = (int) Longship.Instance.ConfigurationManager.Configuration.MaxPlayers;
+        }
+    }
+}

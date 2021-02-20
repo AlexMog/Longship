3fdaeeebd490d9e5a67cf5e949a68b68d9815b0c
AlexMog <alexmog@live.fr>
2021-02-20 20:50:23 +0100

    Listen to character heals

---
+++ Longship/Events/CharacterHealEvent.cs
@@ -0,0 +1,12 @@
+﻿namespace Longship.Events
+{
+    public class CharacterHealEvent : CharacterEvent
+    {
+        public float HealValue { get; set; }
+        
+        public CharacterHealEvent(Character character, float healValue) : base(character)
+        {
+            HealValue = healValue;
+        }
+    }
+}
---
+++ Longship/Patches/PatchListenToCharacterHeal.cs
@@ -0,0 +1,16 @@
+﻿using HarmonyLib;
+using Longship.Events;
+
+namespace Longship.Patches
+{
+    [HarmonyPatch(typeof(Character), "Heal")]
+    public class PatchListenToCharacterHeal
+    {
+        static void Prefix(Character __instance, ref float hp)
+        {
+            var evt = new CharacterHealEvent(__instance, hp);
+            Longship.Instance.EventManager.DispatchEvent(evt);
+            hp = evt.HealValue;
+        }
+    }
+}
